24230406f24a2e40eedd2570f93a5e5c2ab382bb
tracyma1118 <yujiehuang1118@gmail.com>
2023-06-29 13:02:54 +0100

    change: cookie

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const { unparse } = require('papaparse');
 
-const url = 'https://nextdoor.co.uk/neighborhood/castlegreen--kenilworth--england';
+const url = 'https://nextdoor.co.uk/neighborhood/regentspark--london--england/?source=settings';
 const size = 10;
 
 main();
@@ -46,10 +46,22 @@ async function fetchFeeds(
 ) {
     const response = await fetch("https://nextdoor.co.uk/api/gql/ContentFromHoodFeed", {
         "headers": {
+            "accept": "*/*",
+            "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
             "content-type": "application/json",
-            "x-csrftoken": "hsBkarOr4CfOmpoyS6iVP08PuMmY96Cqrdq5bmnEU7N04rV1tuiHizZdMeupVjhb",
-            "cookie": "WE=dce96011-a345-439e-b1cd-99dd15ed4341230619; _gcl_au=1.1.1766645539.1687176565; _pin_unauth=dWlkPU56UmpZbVZtWlRrdE9USTBOeTAwWVdabExXSm1Nakl0TTJZeE5EVXlOV0kwWm1VMA; hubspotutk=52c38bf449bf55ce5c5bff252578c1d3; __hssrc=1; WE3P=dce96011-a345-439e-b1cd-99dd15ed4341230619; spark-nav-onboarding-completion=Mon%20Jun%2019%202023%2020:13:51%20GMT+0800%20(Taipei%20Standard%20Time); role-invitation-decline-notification=false; role-resignation-notification=false; _gid=GA1.3.591372219.1687700987; ln_or=eyIzOTUzMDAiOiJkIn0%3D; seen-tour-in-past-day=true; lang=en-gb; g_state={\"i_l\":1,\"i_t\":1687787579703,\"i_p\":1687708395696}; csrftoken=hsBkarOr4CfOmpoyS6iVP08PuMmY96Cqrdq5bmnEU7N04rV1tuiHizZdMeupVjhb; ndbr_at=oclhs29Zdf5ON-A6svxjp24QijSz7sa-HxzHAS4Rbxg; ndbr_idt=eyJhbGciOiJSUzI1NiIsImtpZCI6ImJhZmU4NTNkLTlmMTktNGJhMy05NTY4LTA1ZTg1MTk0ODFkOSIsInR5cCI6IkpXVCJ9.eyJhdF9oYXNoIjoiV2JIS2YrVWpuenNRRnFXYy9JcWxzZyIsImF1ZCI6WyJuZXh0ZG9vci1kamFuZ28iLCJuZXh0ZG9vciJdLCJleHAiOjE2ODc3ODc2MDQsImlhdCI6MTY4NzcwMTIwNCwiaXNzIjoiaHR0cHM6Ly9hdXRoLm5leHRkb29yLmNvbSIsIm5kX3ByYyI6W3siY291bnRyeSI6IkdCIiwicGlkIjoiMTc1OTIyMDQ3OTE0MTAiLCJ1cmwiOiJodHRwczovL3VzZXIubmV4dGRvb3IuY28udWsvdjEvcHJvZmlsZS8xNzU5MjIwNDc5MTQxMCJ9XSwic3ViIjoiNDc5M2JkMjUtMDQxOS00NDZhLTgxNWMtYjZlYmYwMzBhMDZlIn0.W9MJEUtuaMxJXatqcfdlVNO4hIQZ8UsW9lkEbjZC5-TUN-Yp_4F1S8IJEn7QcE8_15GariQG2vINCu527d2iImFLb3p58DOF_EcjpImfEGFHB2pDMe4m5tabBcXwHdtmHUg9E5gjcbxbb8yxSv6MmMEqRG_3LLzboX5aCqIMZATOBDQSrtkRZGFSJX7U2Y4_0NvrX4w5qU83Z7pLxLp1N05odQtMFUHw0JQlQ4TvvrWrFiHYnOhfMaee515puKHUm-_9fjk_MYHQ3ClHf73dNEhDrpb2_n3hA5071bYykS3jxhfHyA61uibzPNrwiA83R3Tz4f4-gWRT2B_iR0BKFQ; s=kuewedtvjf6cwqcvoy24o1he1pgto315; OptanonConsent=isGpcEnabled=0&datestamp=Mon+Jun+26+2023+21%3A16%3A00+GMT%2B0800+(Taipei+Standard+Time)&version=202209.2.0&isIABGlobal=false&hosts=&consentId=bbbe1495-20a6-4b6f-8f68-a7e0bc0eb167&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0003%3A0%2CC0004%3A0%2CC0002%3A0&AwaitingReconsent=false; _ga=GA1.1.1796352805.1687176565; _ga_L2ES4MTTT0=GS1.1.1687785357.5.1.1687785362.55.0.0; _ga_HRMXJS2LRC=GS1.1.1687785357.5.1.1687785362.0.0.0; _uetsid=25dda540135f11ee88f9f7438610c610; _uetvid=21d2bde00e9a11eebb8345971136d640; __hstc=35119166.52c38bf449bf55ce5c5bff252578c1d3.1687176568926.1687700989453.1687785363498.4; __hssc=35119166.1.1687785363498; WERC=4ec8a138-f9d2-4a64-819a-bb9d825175ef2306261687786487; _dd_s=logs=1&id=e0b53454-fb31-4377-b213-ba40c4b88fa2&created=1687785353191&expire=1687787445846",
-            "Referer": "https://nextdoor.co.uk/neighborhood",
+            "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-csrftoken": "HodRTHdrLVIqMTbLUVt2Vq4aVeRhxUpsnDkSqmiVNh5OFZ3DRmwjkAteSoANyip1",
+            "x-nd-activity-id": "5536E3FE-FF5A-496E-9E00-3B3582C5A896",
+            "x-nd-activity-source": "no-referrer",
+            "x-nd-eid": "",
+            "x-nd-lop": "null",
+            "cookie": "WE=31721309-e808-406b-a45f-aca229b237c1230619; _gcl_au=1.1.1518266440.1687175995; _pin_unauth=dWlkPU1qRXdOV1k1TnpJdE4yVTROeTAwTVdZMkxXSmlOall0TURFeFpqRmtPVE01T1RjeA; hubspotutk=ebe359433854bdc1a0b8129111e8454a; OptanonAlertBoxClosed=2023-06-19T11:59:59.789Z; g_state={\"i_l\":0}; WE3P=31721309-e808-406b-a45f-aca229b237c1230619; spark-nav-onboarding-completion=Sun%20Jun%2025%202023%2014:40:02%20GMT+0100%20(%E8%8B%B1%E5%9C%8B%E5%A4%8F%E4%BB%A4%E6%99%82%E9%96%93); role-invitation-decline-notification=false; role-resignation-notification=false; __hssrc=1; csrftoken=HodRTHdrLVIqMTbLUVt2Vq4aVeRhxUpsnDkSqmiVNh5OFZ3DRmwjkAteSoANyip1; s=ticv58c38yxrdw9c4tzbvz5522oxkyik; ndbr_at=fONkyWtdDVMfq4VohPDWQIKFd3t9iqte_5jys00cPeI; ndbr_idt=eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1ZGYxZDhmLWZjNTEtNDllZC1hOTMxLWQ0MzkyNWUyMDFiNCIsInR5cCI6IkpXVCJ9.eyJhdF9oYXNoIjoiNlp4dXpGblMyUFR0VVd2akZRbktKUSIsImF1ZCI6WyJuZXh0ZG9vci1kamFuZ28iLCJuZXh0ZG9vciJdLCJleHAiOjE2ODgxMjQ3MTQsImlhdCI6MTY4ODAzODMxNCwiaXNzIjoiaHR0cHM6Ly9hdXRoLm5leHRkb29yLmNvbSIsIm5kX3ByYyI6W3siY291bnRyeSI6IkdCIiwicGlkIjoiMTc1OTIyMDQ3NzA4NTkiLCJ1cmwiOiJodHRwczovL3VzZXIubmV4dGRvb3IuY28udWsvdjEvcHJvZmlsZS8xNzU5MjIwNDc3MDg1OSJ9LHsiY291bnRyeSI6IkdCIiwicGlkIjoiMTc1OTIyMDQ4MzQ0NzciLCJ1cmwiOiJodHRwczovL3VzZXIubmV4dGRvb3IuY28udWsvdjEvcHJvZmlsZS8xNzU5MjIwNDgzNDQ3NyJ9XSwic3ViIjoiNDAxNmQ4OWMtNDNjNy00OTA4LTllODEtMjcxMWQ4MDg5MGYwIn0.EluST1TLSpoIv-6U2NeswtROIHBR27U7XkFXy37qVo_QtK_8mNV_wfxebVX3T5aA5mN8_k1SL-v48KRezRSqq_bPxlBx8YX3_LV-hUB5CEfyN2g1tArsxvSbZzeouV1fqKlWQkhEgvnHLqP1Zb1mPDh--leeSA2vFijIxBmVpXj8QUFeleiFyiMtqAX3OTxGoBFxfeDTV4XcubJ9lCEOVGefFfWKhTpsoqcF612l4cWH79Irswb62fntimLQ2e7stHBSJzWDkiE9CxxvYCY2Iy5gMMCo7KImYVejv0mCaR0bItQAXkxNX5Xfk9ZWIa_DThbhnTtLUaPntO7cu57J-A; _gid=GA1.3.2056111007.1688037768; ln_or=eyIzOTUzMDAiOiJkIn0%3D; __hstc=35119166.ebe359433854bdc1a0b8129111e8454a.1687175996005.1687700406579.1688037768802.8; seen-tour-in-past-day=true; WERC=38f4c90e-2d08-4b47-9b14-254588e60f8f2306291688039428; OptanonConsent=isGpcEnabled=0&datestamp=Thu+Jun+29+2023+12%3A41%3A16+GMT%2B0100+(%E8%8B%B1%E5%9C%8B%E5%A4%8F%E4%BB%A4%E6%99%82%E9%96%93)&version=202209.2.0&isIABGlobal=false&hosts=&consentId=95a424e7-4f02-448a-8847-86d35db9de1a&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0003%3A0%2CC0004%3A0%2CC0002%3A0&geolocation=GB%3BENG&AwaitingReconsent=false; _gat_UA-18585915-1=1; _gat_UA-18585915-20=1; _ga=GA1.1.1975139368.1687175996; _ga_L2ES4MTTT0=GS1.1.1688037767.14.1.1688038877.55.0.0; _ga_HRMXJS2LRC=GS1.1.1688037767.14.1.1688038877.0.0.0; _uetsid=46ac2540166f11eea103b35aeb531c23; _uetvid=ce78ea800e9811eeab698b1830bd94af; __hssc=35119166.4.1688037768802; _dd_s=logs=1&id=b6f93c02-91b3-4d06-b0e1-f70a0325c9c4&created=1688037763567&expire=1688039809805",
+            "Referer": "https://nextdoor.co.uk/neighborhood/regentspark--london--england/?source=settings",
             "Referrer-Policy": "strict-origin-when-cross-origin"
         },
         "body": JSON.stringify({
